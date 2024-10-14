302980577f2d35608248145c4922f46e9c0a3f96
Wolffox151 <72281946+Wolffox151@users.noreply.github.com>
2024-10-14 20:09:37 +0300

    Update

--- src/types/index.ts
+++ src/types/index.ts
@@ -12,19 +12,33 @@ export interface IOrder {
   email: string;
   phone: string;
   address: string;
+  checkValidationPayment(data: Record<keyof TCartOrderUserPaymentAdress, string>):boolean;
+  checkValidationContacts(data: Record<keyof TCartOrderUserContacts, string>):boolean;
 }
 
 export interface ICartData {
   total: number;
   items: IProduct['id'][];
   addProduct(product: IProduct['id']): void;
+  removeProduct(product: IProduct['id']): void;
+
 }
 
 export interface IProductData {
   total: number;
   items: IProduct[];
+  preview: string | null;
+  getProduct(productId: string): IProduct;
 }
 
-export type TProductBaseInfo = Pick<IProduct, 'title' | 'category' | 'image' | 'price'>
-export type TProductInfo = Pick<IProduct, 'title'  | 'description' | 'category' | 'image' | 'price'>
+export type TProductBaseInfo = Pick<IProduct, 'title' | 'category' | 'image' | 'price'>;
+export type TProductInfo = Pick<IProduct, 'title'  | 'description' | 'category' | 'image' | 'price'>;
+
+export type TCartOrderProducts = Pick<ICartData, 'total' | 'items'>;
+export type TCartOrderUserPaymentAdress = Pick<IOrder, 'payment' | 'address'>;
+export type TCartOrderUserContacts = Pick<IOrder, 'email' | 'phone'>;
+export type TCartOrderSuccess = Pick<ICartData, 'total'>;
+
+
+
 
